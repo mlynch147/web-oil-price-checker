4458c9bddef5fe25147c0638c48d52d6d8f26208
Martin Lynch <martin.lynch1@nhs.net>
2024-08-30 17:26:09 +0100

    passing different amounts - bug in the lower amounts as not all suppliers offer 100 litres...

--- src/main/java/com/ml/oilpricechecker/controllers/PriceController.java
+++ src/main/java/com/ml/oilpricechecker/controllers/PriceController.java
@@ -6,6 +6,7 @@ import com.ml.oilpricechecker.models.PriceResponse;
 import com.ml.oilpricechecker.service.PriceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -22,12 +23,12 @@ public class PriceController {
         this.priceService = priceService;
     }
 
-     @GetMapping("/prices")
-     public List<Price> getPrices() throws Exception {
+     @GetMapping("/prices/{litre}")
+     public List<Price> getPrices(@PathVariable("litre") final String litre) throws Exception {
          List<Price> data = new ArrayList<>();
 
          List<PriceResponse> pricesResponses =
-                 priceService.getCurrentPrices(DEFAULT_NUMBER_OF_LITRES);
+                 priceService.getCurrentPrices(Integer.valueOf(litre));
 
          for (PriceResponse priceResponse: pricesResponses) {
              data.add(PriceMapper.mapPriceResponseToPrice(priceResponse));
