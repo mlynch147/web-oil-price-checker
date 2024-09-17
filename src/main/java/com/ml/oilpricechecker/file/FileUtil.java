b93acac4d837d8a1862cdc94ef11ab640b32795a
Martin Lynch <martin.lynch1@nhs.net>
2024-09-17 11:43:51 +0100

    moving files to writable directory.... first pass of writing to the files

--- src/main/java/com/ml/oilpricechecker/file/FileUtil.java
+++ src/main/java/com/ml/oilpricechecker/file/FileUtil.java
@@ -1,15 +1,12 @@
 package com.ml.oilpricechecker.file;
 
-import org.springframework.core.io.ClassPathResource;
-
 import java.io.BufferedReader;
+
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,9 +21,15 @@ public final class FileUtil {
     public static final int MAX_CHART_DATA_DAYS = 14;
     public static final int MAX_WEEKLY_COMPARISON_DAYS = 8;
 
+    private static final String BASE_PATH = "src/main/data/";
+
+
     // Method to write data to a file in internal storage
     public static void writeToFile(final String filename, final String newDate, final String newAmount) {
         synchronized (LOCK) {
+
+            Path filePath = Paths.get(BASE_PATH, filename);
+
             List<FileData> dataList = getCurrentFileContent(filename);
 
             String newDateTrimmed = newDate.trim();
@@ -61,8 +64,8 @@ public final class FileUtil {
                 dataList.add(new FileData(newDateTrimmed, newAmountTrimmed));
             }
 
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-                // Write each entry to the file
+            try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
+                // Loop through the dataList and write each key-value pair to the file
                 for (FileData data : dataList) {
                     String line = data.getDate() + "=" + data.getAmount();
                     writer.write(line);
@@ -74,93 +77,51 @@ public final class FileUtil {
         }
     }
 
-    // Method to read data from a file in internal storage
-    private static List<FileData> getCurrentFileContent(final String filename) {
-        synchronized (LOCK) {
-            List<FileData> dataList = new ArrayList<>();
 
-            // Check if the file exists before reading its content
-            File file = new File(filename);
-            if (!file.exists()) {
-                // Create an empty file if it doesn't exist
-                try {
-                    file.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                // Read from the existing file
-                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        FileData data = parseFileData(line);
-                        dataList.add(data);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+    public static List<FileData> getCurrentFileContent(final String filename) {
+        List<FileData> dataList = new ArrayList<>();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
 
-            return dataList;
-        }
-    }
+        try {
+            Path filePath = Paths.get(BASE_PATH, filename);
 
+            try (BufferedReader reader = Files.newBufferedReader(filePath)) {
 
-    // Method to parse a string into ChartData
-    private static FileData parseFileData(final String dataString) {
-        // Assuming the format is "dd/MM/yyyy=value"
-        String[] parts = dataString.split("=");
-        String dateString = parts[0].trim();
-        String value = parts[1].trim();
-        return new FileData(dateString, value);
-    }
-
-
-    // Method to read data from a file in internal storage and create a list of ChartData objects
-    public static List<PriceDataPoint> readFromFile(final String filename) {
-        synchronized (LOCK) {
-            List<PriceDataPoint> dataList = new ArrayList<>();
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
-
-            try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    String[] parts = line.split("=");
-                    if (parts.length == 2) {
-                        String dateString = parts[0];
-                        double value = Double.parseDouble(parts[1]);
+                    String[] keyValue = line.split("=");
 
-                        // Assuming you have a constructor in ChartData that takes a Date and an int
-                        PriceDataPoint priceDataPoint = new PriceDataPoint(dateFormat.parse(dateString), value);
-
-                        dataList.add(priceDataPoint);
-                    }
+                    dataList.add(new FileData(keyValue[0], keyValue[1]));
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-
-            return dataList;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        return dataList;
     }
 
+
+    //Essentially a duplicate of the method above... could remove this and do the PriceDataPoint somewhere else...  ChartService perhaps...
     public static SupplierPriceData readDataFromFile(final String fileName, final String displayName) {
         List<PriceDataPoint> dataList = new ArrayList<>();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
 
         try {
-            ClassPathResource resource = new ClassPathResource("data/" + fileName);
-            InputStream inputStream = resource.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            Path filePath = Paths.get(BASE_PATH, fileName);
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] keyValue = line.split("=");
-                String date = keyValue[0];
-                double value = Double.parseDouble(keyValue[1]);
+            try (BufferedReader reader = Files.newBufferedReader(filePath)) {
 
-                PriceDataPoint priceDataPoint = new PriceDataPoint(dateFormat.parse(date), value);
-                dataList.add(priceDataPoint);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] keyValue = line.split("=");
+                    String date = keyValue[0];
+                    double value = Double.parseDouble(keyValue[1]);
+
+                    PriceDataPoint priceDataPoint =
+                            new PriceDataPoint(dateFormat.parse(date), value);
+                    dataList.add(priceDataPoint);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/com/ml/oilpricechecker/service/FileWriterService.java
+++ src/main/java/com/ml/oilpricechecker/service/FileWriterService.java
@@ -1,9 +1,13 @@
 package com.ml.oilpricechecker.service;
 
+import com.ml.oilpricechecker.file.FileUtil;
 import com.ml.oilpricechecker.models.Price;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.io.FileWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -19,12 +23,31 @@ public class FileWriterService {
         // For instance, using a BufferedWriter or Files API
         try {
             // Simulate file writing with a sleep
-            Thread.sleep(MILLIS);
             System.out.println("Writing data to file...");
+            String date = getDateAsString();
+
+            for (Price price: prices) {
+                if (price.getSupplierName().toLowerCase().contains("craig")) {
+                    FileUtil.writeToFile("craigs.txt", date, price.getPrice().substring(1));
+                }
+                if (price.getSupplierName().toLowerCase().contains("scott")) {
+                    FileUtil.writeToFile("scotts.txt", date, price.getPrice().substring(1));
+                }
+                if (price.getSupplierName().toLowerCase().contains("campsie")) {
+                    FileUtil.writeToFile("campsie.txt", date, price.getPrice().substring(1));
+                }
+            }
             // Actual file writing logic here
         } catch (Exception e) {
             // Handle exception
             e.printStackTrace();
         }
+    }
+
+    private String getDateAsString() {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = today.format(formatter);
+        return formattedDate;
     }
 }
