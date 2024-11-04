983f274346c4cbd1af5eea78e589d3efd817d664
Martin Lynch <martin.lynch1@nhs.net>
2024-11-04 11:43:18 +0000

    added a 5 second timeout to the http requests

--- src/main/java/com/ml/oilpricechecker/fetcher/PostPriceFetcher.java
+++ src/main/java/com/ml/oilpricechecker/fetcher/PostPriceFetcher.java
@@ -8,15 +8,28 @@ import com.ml.oilpricechecker.util.PriceUtilities;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 public class PostPriceFetcher implements PriceFetcher {
+
+    public static final int MAX_TIMEOUT_IN_MILLISECONDS = 5000;
     private final RestTemplate restTemplate;
 
     public PostPriceFetcher(final RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+        setTimeouts();
+    }
+
+    private void setTimeouts() {
+        if (this.restTemplate.getRequestFactory() instanceof SimpleClientHttpRequestFactory) {
+            SimpleClientHttpRequestFactory requestFactory
+                    = (SimpleClientHttpRequestFactory) this.restTemplate.getRequestFactory();
+            requestFactory.setConnectTimeout(MAX_TIMEOUT_IN_MILLISECONDS);
+            requestFactory.setReadTimeout(MAX_TIMEOUT_IN_MILLISECONDS);
+        }
     }
 
     @Override
